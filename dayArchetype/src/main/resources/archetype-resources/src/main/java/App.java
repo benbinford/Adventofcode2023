26bd2e6f8adcc1be4094d3727b12cfac9d921406
benbinford <ben.binford@gmail.com>
2023-12-04 18:13:37 -0500

    Add default to load input file in archetype

--- dayArchetype/src/main/resources/archetype-resources/src/main/java/App.java
+++ dayArchetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,5 +1,7 @@
 package $package;
 
+import com.benjaminbinford.utils.IO;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final var input = IO.getResource("${package.replace('.', '/')}/input.txt");
+
     }
 }
